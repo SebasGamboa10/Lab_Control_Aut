9e27c35d177767c0301339e4561e5928be6b2480
SebasGamboa10 <segamboachacon@gmail.com>
2021-10-18 21:33:52 -0600

    Prueba controlador REI

--- Grua/Retro_Alimentacion_Integral.m
+++ Grua/Retro_Alimentacion_Integral.m
@@ -8,10 +8,10 @@ s=tf('s');
 %csys = canon(sys,'companion')
 
 
-A = Model_Grua.A;
-B = Model_Grua.B;
-C = Model_Grua.C;
-D = Model_Grua.D;
+A = Model_Grua_FCC.A;
+B = Model_Grua_FCC.B;
+C = Model_Grua_FCC.C;
+D = Model_Grua_FCC.D;
 
 %Polos
 u1=-3.5;
@@ -20,18 +20,17 @@ u3=-8.4*100;
 u4=-1.5*150;
 u5=-1*1500;
 u6=-10.5*205;
-P=[u1, u2, u3, u4, u5, u6];
+P=[u1, u2, u3, u4, u5];
 
-M_crt = [A B;-C [0;0]]
+M_crt = [A B;-C [0 0]]
 
 
-A_t = [A [0;0;0;0];-C [0;0]];
-A_tt = A_t*A_t';
-B_t = [B;0;0];
+A_t = [A [0;0;0;0];-C [0]];
+B_t = [B;0 0];
 %M=[B_t (A_t*B_t) ((A_t^2)*B_t)]
 
 %Sacando K, y ki
-Ks = place(A_tt, B_t,P)
+Ks = place(A_t, B_t,P)
 
 K_REI=[Ks(:,1) Ks(:,2)]
 Ki_REI = -Ks(:,3)
