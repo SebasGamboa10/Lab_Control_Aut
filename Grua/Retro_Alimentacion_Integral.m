d5cdc630295c2804c3e3ca303c81c609a06d4c73
Gesla24 <gerald-se@hotmail.com>
2021-10-19 17:23:17 -0600

    Actualizaciones

--- Grua/Retro_Alimentacion_Integral.m
+++ Grua/Retro_Alimentacion_Integral.m
@@ -8,10 +8,10 @@ s=tf('s');
 %csys = canon(sys,'companion')
 
 
-A = Model_Grua_FCC.A;
-B = Model_Grua_FCC.B;
-C = Model_Grua_FCC.C;
-D = Model_Grua_FCC.D;
+A = Grua_ss.A;
+B = Grua_ss.B;
+C = Grua_ss.C;
+D = Grua_ss.D;
 
 %Polos
 u1=-3.5;
@@ -19,15 +19,14 @@ u2=-3*10;
 u3=-8.4*100;
 u4=-1.5*150;
 u5=-1*1500;
-u6=-10.5*205;
 P=[u1, u2, u3, u4, u5];
 
-M_crt = [A B;-C [0 0]]
+%M_crt = [A B;-C 0]
 
 
-A_t = [A [0;0;0;0];-C [0]];
-B_t = [B;0 0];
-%M=[B_t (A_t*B_t) ((A_t^2)*B_t)]
+A_t = [A [0;0;0;0];-C [0;0]];
+B_t = [B;0];
+M=[B_t (A_t*B_t) ((A_t^2)*B_t) ((A_t^3)*B_t)]
 
 %Sacando K, y ki
 Ks = place(A_t, B_t,P)
