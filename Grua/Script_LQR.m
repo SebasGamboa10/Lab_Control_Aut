01696a2950a1c1e80f51f3139af5f2300223f518
Gesla24 <gerald-se@hotmail.com>
2021-10-20 19:21:45 -0600

    Controles

--- Grua/Script_LQR.m
+++ Grua/Script_LQR.m
@@ -1,11 +1,11 @@
 % --> LQR <---
 
-A = [-0.1456 4.141; -4.141 -0.1456];
-B = [0;1.966];
-C = [1.388 0];
-D = 0;
+A = Grua_ss.A;
+B = Grua_ss.B;
+C = [1 0 0 0];
+D = Grua_ss.D;
 
-Q = 1/2*[2.5 0 0; 0 0.01 0; 0 0 3.5];
+Q = 1/2*[1 0 0 0 0; 0 1 0 0 0; 0 0 1 0 0; 0 0 0 1 0; 0 0 0 0 1];
 R = 1/2*2.5;
 
 %K = lqr(A,B,Q,R);
@@ -13,13 +13,13 @@ R = 1/2*2.5;
 %ceros es matriz de ceros
 ceros=zeros(length(A),1); %El numero de columnas de la matriz de ceros
 
-As = [A ceros; -C D];
-Bs = [B; D];
+As = [A ceros; -C 0];
+Bs = [B; 0];
 
 Ks = lqr(As,Bs,Q,R)
 
 %Siempre hay que analizar el caso
-K_LQR = Ks(:,[1:2])
-Ki_LQR = -Ks(:,[3])
+K_LQR = Ks(:,[1:4])
+Ki_LQR = -Ks(:,[5])
 
 
