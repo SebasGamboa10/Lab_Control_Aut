88ec09524286cde1bd92662457cf15d48271b382
SebasGamboa10 <segamboachacon@gmail.com>
2021-11-09 17:30:40 -0600

    Cambios

--- Helicóptero/Script_LQR.m
+++ Helicóptero/Script_LQR.m
@@ -5,16 +5,16 @@ B = Heli_ss.B;
 C = Heli_ss.C;
 D = Heli_ss.D;
 
-Q = [1 0 0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0; 0 0 0 1 0 0; 0 0 0 0 1 0;0 0 0 0 0 1];
+Q = [800 0 0 0 0 0; 0 200 0 0 0 0; 0 0 200 0 0 0; 0 0 0 1 0 0; 0 0 0 0 1 0;0 0 0 0 0 100];
 R = 1;
 
 %K = lqr(A,B,Q,R);
 
 %ceros es matriz de ceros
-ceros=zeros(length(A),1); %El numero de columnas de la matriz de ceros
+ceros=zeros(length(A),2); %El numero de columnas de la matriz de ceros
 
 As = [A ceros; -C [0 0; 0 0]];
-Bs = [B; 0];
+Bs = [B; [0 0;0 0]];
 
 Ks = lqr(As,Bs,Q,R)
 
